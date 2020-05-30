da4fc7d07c1072db5ea12f16d76193024b5ce390
anhnt <anhntbk08@gmail.com>
2020-05-30 11:15:02 +0700

    init project

---
+++ internal/app/todocli/context.go
@@ -0,0 +1,13 @@
+package todocli
+
+import (
+	todov1beta1 "gateway/.gen/api/proto/todo/v1beta1"
+)
+
+type context struct {
+	client todov1beta1.TodoListClient
+}
+
+func (c *context) GetTodoClient() todov1beta1.TodoListClient {
+	return c.client
+}
